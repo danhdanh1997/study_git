fe636c5f9f3f117259eecfc3632e52a348d949e5
xuandanh <56081344+danhdanh1997@users.noreply.github.com>
2022-02-13 14:14:40 +0700

    Update demo.java

--- src/org/com/java/demo.java
+++ src/org/com/java/demo.java
@@ -3,7 +3,7 @@ package org.com.java;
 public class demo {
     public static void main(String[] args) {
         System.out.println("hello master ");
-
+         System.out.println("########################## ");
         System.out.println("hello xuan danh");
     }
 }
